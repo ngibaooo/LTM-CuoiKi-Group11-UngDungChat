c7ad2e6e9b778c12c4d4211e4f2207d35099ddcf
GiaBaoo <ngbao840@gmail.com>
2025-08-13 10:56:03 +0700

    update config.py, get db information from .env for security

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-DB_HOST = "192.168.2.44"
-DB_USER = "root"
-DB_PASSWORD = "0907986078bao"
-DB_NAME = "chat_app"
+import os
+from dotenv import load_dotenv
+
+# Load file .env
+load_dotenv()
+
+# Lấy thông tin DB từ biến môi trường .env
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT", 3306)) #nếu không có port mặc định sẽ là 3306
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
